5329143af007c27b53f13409a74be5807ffffc4a
Ilya Dobryakov <idolmaster1707@gmail.com>
2021-11-21 23:41:55 +0300

    new stories widgetbook

--- stories/manga_list_stories.dart
+++ stories/manga_list_stories.dart
@@ -1,39 +1,71 @@
 import 'package:flutter/material.dart';
 import 'package:manga_reader/manga_list/manga_list.dart';
 import 'package:manga_reader/manga_list/widget/manga.dart';
+import 'package:widgetbook/widgetbook.dart';
 
 final List<Manga> mangaList = [
-  const Manga(
-      title: "Demon Incidents (Инцидент Кэмоно)",
-      url: "https://imgcover.manga-chan.me/showfull_retina/uploads/posts/2017-03/thumbs/1490629254_00a.jpg",
-      rate: 8),
+  Manga(
+    title: "Demon Incidents (Инцидент Кэмоно)",
+    url:
+        "https://imgcover.manga-chan.me/showfull_retina/uploads/posts/2017-03/thumbs/1490629254_00a.jpg",
+    rate: 8,
+    onClick: () {
+      print("a");
+    },
+  ),
   const Manga(
       title: "Ijiranaide, Nagatoro-san (Не издевайся, Нагаторо-сан)",
-      url: "https://imgcover.manga-chan.me/showfull_retina/uploads/posts/2017-11/thumbs/1510928199_f-list.jpg",
+      url:
+          "https://imgcover.manga-chan.me/showfull_retina/uploads/posts/2017-11/thumbs/1510928199_f-list.jpg",
       rate: 24),
   const Manga(
-      title: "Sword Art Online: Project Alicization (Искусство Меча Онлайн: Проект «Алисизация»)",
-      url: "https://imgcover.manga-chan.me/showfull_retina/uploads/posts/2014-02/thumbs/1392293364_i177955.jp"),
+      title:
+          "Sword Art Online: Project Alicization (Искусство Меча Онлайн: Проект «Алисизация»)",
+      url:
+          "https://imgcover.manga-chan.me/showfull_retina/uploads/posts/2014-02/thumbs/1392293364_i177955.jp"),
 ];
 
 final List<Manga> mangaRead = [
   const Manga(
       title: "Demon Incidents (Инцидент Кэмоно)",
-      url: "https://imgcover.manga-chan.me/showfull_retina/uploads/posts/2017-03/thumbs/1490629254_00a.jpg",
+      url:
+          "https://imgcover.manga-chan.me/showfull_retina/uploads/posts/2017-03/thumbs/1490629254_00a.jpg",
       last: 5),
   const Manga(
       title: "Ijiranaide, Nagatoro-san (Не издевайся, Нагаторо-сан)",
-      url: "https://imgcover.manga-chan.me/showfull_retina/uploads/posts/2017-11/thumbs/1510928199_f-list.jpg",
+      url:
+          "https://imgcover.manga-chan.me/showfull_retina/uploads/posts/2017-11/thumbs/1510928199_f-list.jpg",
       last: 24),
   const Manga(
       title: "The World God only Knows (Одному лишь Богу ведомый мир)",
-      url: "https://imgcover.manga-chan.me/showfull_retina/uploads/posts/2012-07/thumbs/1342196249_i133139.jpg",
+      url:
+          "https://imgcover.manga-chan.me/showfull_retina/uploads/posts/2012-07/thumbs/1342196249_i133139.jpg",
       last: 120),
   const Manga(
     title: "Land of the Lustrous (Страна самоцветов)",
-    url: "https://imgcover.manga-chan.me/showfull_retina/uploads/posts/2014-02/thumbs/1392293364_i177955.jpg",
+    url:
+        "https://imgcover.manga-chan.me/showfull_retina/uploads/posts/2014-02/thumbs/1392293364_i177955.jpg",
   ),
 ];
 
-Widget listing() => Center(child: MangaList(mangaList: mangaList));
-Widget reading() => Center(child: MangaList(mangaList: mangaRead));
+WidgetbookWidget mangaListStories() => WidgetbookWidget(
+      name: 'manga list',
+      useCases: [
+        WidgetbookUseCase(
+          name: 'listing',
+          builder: (context) => Center(
+            child: MangaList(
+              mangaList: mangaList,
+            ),
+          ),
+        ),
+        WidgetbookUseCase(
+          name: 'reading',
+          builder: (context) => Center(
+            child: MangaList(
+              mangaList: mangaRead,
+            ),
+          ),
+        ),
+      ],
+    );
--- stories/manga_stories.dart
+++ stories/manga_stories.dart
@@ -1,32 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:manga_reader/manga_list/widget/manga.dart';
+import 'package:widgetbook/widgetbook.dart';
 
-Widget common() => const Center(
-      child: Manga(
-        title: "Demon Incidents (Инцидент Кэмоно)",
-        url: "https://imgcover.manga-chan.me/showfull_retina/uploads/posts/2017-03/thumbs/1490629254_00a.jpg",
-      ),
-    );
-
-Widget rating() => const Center(
-      child: Manga(
-        title: "Demon Incidents (Инцидент Кэмоно)",
-        url: "https://imgcover.manga-chan.me/showfull_retina/uploads/posts/2017-03/thumbs/1490629254_00a.jpg",
-        rate: 1234,
-      ),
-    );
-
-Widget reading() => const Center(
-      child: Manga(
-        title: "Demon Incidents (Инцидент Кэмоно)",
-        url: "https://imgcover.manga-chan.me/showfull_retina/uploads/posts/2017-03/thumbs/1490629254_00a.jpg",
-        last: 1234,
-      ),
-    );
-
-Widget noUrl() => const Center(
-      child: Manga(
-        title: "Sword Art Online: Project Alicization (Искусство Меча Онлайн: Проект «Алисизация»)",
-        url: "",
-      ),
+WidgetbookWidget mangaStories() => WidgetbookWidget(
+      name: 'manga cover',
+      useCases: [
+        WidgetbookUseCase(
+          name: 'default',
+          builder: (context) => const Center(
+            child: Manga(
+              title: "Demon Incidents (Инцидент Кэмоно)",
+              url:
+                  "https://imgcover.manga-chan.me/showfull_retina/uploads/posts/2017-03/thumbs/1490629254_00a.jpg",
+            ),
+          ),
+        ),
+        WidgetbookUseCase(
+          name: 'reading',
+          builder: (context) => const Center(
+            child: Manga(
+              title: "Demon Incidents (Инцидент Кэмоно)",
+              url:
+                  "https://imgcover.manga-chan.me/showfull_retina/uploads/posts/2017-03/thumbs/1490629254_00a.jpg",
+              last: 1234,
+            ),
+          ),
+        ),
+        WidgetbookUseCase(
+          name: 'rating',
+          builder: (context) => const Center(
+            child: Manga(
+              title: "Demon Incidents (Инцидент Кэмоно)",
+              url:
+                  "https://imgcover.manga-chan.me/showfull_retina/uploads/posts/2017-03/thumbs/1490629254_00a.jpg",
+              rate: 1234,
+            ),
+          ),
+        ),
+        WidgetbookUseCase(
+          name: 'broken url',
+          builder: (context) => const Center(
+            child: Manga(
+              title:
+                  "Sword Art Online: Project Alicization (Искусство Меча Онлайн: Проект «Алисизация»)",
+              url: "",
+            ),
+          ),
+        ),
+      ],
     );
