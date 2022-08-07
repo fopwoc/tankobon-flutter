b5c3392f9d1a2e2eeacdcc58a34cf90294ab4f32
Ilya Dobryakov <aspirin@govno.tech>
2022-08-07 23:34:35 +0300

    mobx for login form

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -1,8 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
-import 'package:tankobon/domain/repositories/login.dart';
-import 'package:tankobon/router/router.dart';
 import 'package:tankobon/widgets/forms/login.dart';
 
 class LoginView extends StatelessWidget {
@@ -16,14 +14,9 @@ class LoginView extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text('login ${context.router.current.name}'),
-            Padding(
-              padding: const EdgeInsets.all(32),
-              child: LoginForm(
-                callback: (v) async {
-                  await loginInRepository(context, v);
-                  await context.router.replace(const DeciderViewRoute());
-                },
-              ),
+            const Padding(
+              padding: EdgeInsets.all(32),
+              child: LoginForm(),
             ),
           ],
         ),
