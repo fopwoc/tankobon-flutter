104022148788b2513784e161a1539daf21c44c8c
Ilya Dobryakov <ilya.dobryakov@icloud.com>
2022-08-07 23:35:12 +0300

    logo in login view

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -1,6 +1,8 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:tankobon/gen/assets.gen.dart';
 import 'package:tankobon/widgets/forms/login.dart';
 
 class LoginView extends StatelessWidget {
@@ -13,6 +15,10 @@ class LoginView extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            SvgPicture.asset(
+              Assets.svg.logo.path,
+              height: 160,
+            ),
             Text('login ${context.router.current.name}'),
             const Padding(
               padding: EdgeInsets.all(32),
